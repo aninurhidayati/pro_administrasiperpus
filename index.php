45e839e4297a700dbd78296df4a6c320506c2701
MAtthaariqSulthan-t <yogaperdana78@gmail.com>
2022-04-26 09:12:29 +0700

    comt

--- index.php
+++ index.php
@@ -12,17 +12,15 @@
   <div class="container d-flex justify-content-center mt-5 pt-5">
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="" class="form-label"><h4>Form Login</h4></label>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Username</label>
+        <input type="email" class="form-control" id="">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <input type="password" class="form-control" id="">
       </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>      
